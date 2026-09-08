a49d4b4f68433d7b70cab30e4318429bfdca97cf
Tauseef666-ctrl <302380325+Tauseef666-ctrl@users.noreply.github.com>
2026-09-09 03:33:07 +0530

    Bundle body images as same-origin assets to fix moon textures and photo icons

---
+++ src/data/bodyAssets.ts
@@ -0,0 +1,83 @@
+import mercury from '../assets/body/mercury.jpg'
+import venus from '../assets/body/venus.jpg'
+import earth from '../assets/body/earth.jpg'
+import mars from '../assets/body/mars.png'
+import jupiter from '../assets/body/jupiter.png'
+import saturn from '../assets/body/saturn.png'
+import uranus from '../assets/body/uranus.png'
+import neptune from '../assets/body/neptune.png'
+import moon from '../assets/body/moon.jpg'
+import phobos from '../assets/body/phobos.jpg'
+import deimos from '../assets/body/deimos.jpg'
+import io from '../assets/body/io.jpg'
+import europa from '../assets/body/europa.jpg'
+import ganymede from '../assets/body/ganymede.jpg'
+import callisto from '../assets/body/callisto.jpg'
+import titan from '../assets/body/titan.jpg'
+import enceladus from '../assets/body/enceladus.png'
+import triton from '../assets/body/triton.jpg'
+import titania from '../assets/body/titania.jpg'
+import oberon from '../assets/body/oberon.jpg'
+import Pluto from '../assets/body/Pluto.png'
+import Charon from '../assets/body/Charon.png'
+import Eris from '../assets/body/Eris.jpg'
+import Haumea from '../assets/body/Haumea.png'
+import Makemake from '../assets/body/Makemake.jpg'
+import C_67P from '../assets/body/67P.jpg'
+import Ceres from '../assets/body/Ceres.jpg'
+import Vesta from '../assets/body/Vesta.jpg'
+import Pallas from '../assets/body/Pallas.png'
+import Hygiea from '../assets/body/Hygiea.jpg'
+import Eros from '../assets/body/Eros.jpg'
+import Bennu from '../assets/body/Bennu.png'
+import Ryugu from '../assets/body/Ryugu.png'
+import Itokawa from '../assets/body/Itokawa.jpg'
+import Psyche from '../assets/body/Psyche.png'
+import Ida from '../assets/body/Ida.jpg'
+import Mathilde from '../assets/body/Mathilde.png'
+import Steins from '../assets/body/Steins.jpg'
+import Lutetia from '../assets/body/Lutetia.jpg'
+import Aten from '../assets/body/Aten.png'
+
+export const BODY_IMAGES: Record<string, string> = {
+  mercury,
+  venus,
+  earth,
+  mars,
+  jupiter,
+  saturn,
+  uranus,
+  neptune,
+  moon,
+  phobos,
+  deimos,
+  io,
+  europa,
+  ganymede,
+  callisto,
+  titan,
+  enceladus,
+  triton,
+  titania,
+  oberon,
+  Pluto,
+  Charon,
+  Eris,
+  Haumea,
+  Makemake,
+  '67P': C_67P,
+  Ceres,
+  Vesta,
+  Pallas,
+  Hygiea,
+  Eros,
+  Bennu,
+  Ryugu,
+  Itokawa,
+  Psyche,
+  Ida,
+  Mathilde,
+  Steins,
+  Lutetia,
+  Aten,
+}
--- src/data/planetTextures.ts
+++ src/data/planetTextures.ts
@@ -1,4 +1,6 @@
-﻿const THREEJS_BASE = 'https://threejs.org/examples/textures/planets'
+﻿import { BODY_IMAGES } from './bodyAssets'
+
+const THREEJS_BASE = 'https://threejs.org/examples/textures/planets'
 const WIKI = 'https://commons.wikimedia.org/wiki/Special:FilePath'
 
 export const PLANET_TEXTURES: Record<string, { map: string; clouds?: string; night?: string; bump?: string; normal?: string; emissive?: string }> = {
@@ -30,19 +32,17 @@ export const PLANET_TEXTURES: Record<string, { map: string; clouds?: string; nig
 
 export const MOON_TEXTURE_URL = `${THREEJS_BASE}/moon_1024.jpg`
 
-const COMMONS = 'https://upload.wikimedia.org/wikipedia/commons'
-
 export const MOON_TEXTURES: Record<string, string> = {
-  moon: `${THREEJS_BASE}/moon_1024.jpg`,
-  phobos: `${COMMONS}/5/5c/Phobos_colour_2008.jpg`,
-  deimos: `${COMMONS}/8/8d/Deimos-MRO.jpg`,
-  io: `${COMMONS}/7/7b/Io_highest_resolution_true_color.jpg`,
-  europa: `${COMMONS}/5/54/Europa-moon.jpg`,
-  ganymede: `${COMMONS}/f/f2/Ganymede_g1_true-edit1.jpg`,
-  callisto: `${COMMONS}/e/e9/Callisto.jpg`,
-  titan: `${COMMONS}/4/45/Titan_in_true_color.jpg`,
-  enceladus: `https://upload.wikimedia.org/wikipedia/commons/1/1a/Enceladus_-_April_14_2012_%2851858537468%29.png`,
-  triton: `https://upload.wikimedia.org/wikipedia/commons/9/91/Triton_%28moon%29.jpg`,
-  titania: `${COMMONS}/4/42/Titania.jpg`,
-  oberon: `${COMMONS}/2/22/Oberon.jpg`,
+  moon: MOON_TEXTURE_URL,
+  phobos: BODY_IMAGES.phobos,
+  deimos: BODY_IMAGES.deimos,
+  io: BODY_IMAGES.io,
+  europa: BODY_IMAGES.europa,
+  ganymede: BODY_IMAGES.ganymede,
+  callisto: BODY_IMAGES.callisto,
+  titan: BODY_IMAGES.titan,
+  enceladus: BODY_IMAGES.enceladus,
+  triton: BODY_IMAGES.triton,
+  titania: BODY_IMAGES.titania,
+  oberon: BODY_IMAGES.oberon,
 }
